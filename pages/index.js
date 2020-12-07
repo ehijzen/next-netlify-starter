0dbd6ee3479c3b48b4753f1aa3a240cabc18aee3
Eric Hijzen <eric@hijzenconsultancy.com>
2020-12-07 20:31:01 +0100

    Update of Mon Dec  7 20:30:46 CET 2020

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Nothing here yet. Just wait and see.... maybe one day...
         </p>
       </main>
 
